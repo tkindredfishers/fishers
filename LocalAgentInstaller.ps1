a63dc39c9aecbb75b937ba2975a0aadfe5614107
tkindredfishers <162155768+tkindredfishers@users.noreply.github.com>
2024-04-25 18:13:49 -0600

    Update LocalAgentInstaller.ps1

--- LocalAgentInstaller.ps1
+++ LocalAgentInstaller.ps1
@@ -5,4 +5,4 @@ Invoke-WebRequest "https://cloudiwaycdn.z13.web.core.windows.net/master/Cloudiwa
 
 Start-Process C:\Windows\System32\msiexec.exe -ArgumentList "/i $tmp TARGETDIR=$cdw /qn COMPANYKEY=e446415e-74f5-42cf-9779-0e05e8846043;https://fisherstechapi.cloudiway.com" -Wait
 
-Start-Process -FilePath "CloudiwayAgent.exe" -WorkingDirectory "$cdw" -Verb runAs
+Start-Process -FilePath "CloudiwayAgent.exe" -WorkingDirectory $cdw -Verb runAs
